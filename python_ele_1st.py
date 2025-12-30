ff33ea3507453e419c2ed42a34e72d0977b0376e
taotiandahui <taotiandahui@gmail.com>
2025-12-30 18:25:03 +0900

    変更

--- python_ele_1st.py
+++ python_ele_1st.py
@@ -1,1 +1,28 @@
-print(1+1)
+import random
+
+kuji = ["大吉","中吉","小吉"]
+
+kuji.append("凶")
+kuji.append("大凶")
+kuji.append("趙大吉")
+
+# print(kuji)
+
+# print("追加後のおみくじ一覧:",kuji)
+print("追加後のおみくじ一覧:",", ".join(kuji))
+print("ここからランダムでおみくじを引きます")
+
+results = []
+results_daikichi = 0
+
+for i in range(5):
+    result = random.choice(kuji)
+    print(f"{i+1}回目の結果: {result}")
+    results.append(result)
+
+    if result == "大吉":
+        results_daikichi += 1
+
+print("---- 抽選結果まとめ ----")
+print(results)
+print(f"大吉回数: {results_daikichi}")
